3edc352203439849e70b339efcde51de9feeb907
Keith James <kdj@sanger.ac.uk>
2015-10-21 16:08:39 +0100

    Test tag 0.

--- t/lib/WTSI/NPG/HTS/HTSFileDataObjectTest.pm
+++ t/lib/WTSI/NPG/HTS/HTSFileDataObjectTest.pm
@@ -7,7 +7,7 @@ use File::Spec;
 use File::Temp;
 use List::AllUtils qw(each_array);
 use Log::Log4perl;
-use Test::More tests => 60;
+use Test::More tests => 76;
 
 use base qw(Test::Class);
 
@@ -32,7 +32,9 @@ my $fixture_counter = 0;
 my $data_path = './t/data';
 my $fixture_path = "$data_path/fixtures";
 
-my $data_file = '3002_3#1';
+my $data_file0 = '3002_3#0';
+my $data_file1 = '3002_3#1';
+
 my $reference_file = 'test_ref.fa';
 my $irods_tmp_coll;
 my $samtools = `which samtools`;
@@ -65,24 +67,26 @@ sub setup_fixture : Test(setup) {
   }
 
   if ($samtools) {
-    WTSI::NPG::HTS::Samtools->new
-        (arguments => ['view', '-C',
-                       '-T', qq[$data_path/$reference_file],
-                       '-o', qq[irods:$irods_tmp_coll/$data_file.cram]],
-         path      => "$data_path/$data_file.sam")->run;
+    foreach my $data_file ($data_file0, $data_file1) {
+      WTSI::NPG::HTS::Samtools->new
+          (arguments => ['view', '-C',
+                         '-T', qq[$data_path/$reference_file],
+                         '-o', qq[irods:$irods_tmp_coll/$data_file.cram]],
+           path      => "$data_path/$data_file.sam")->run;
 
-    WTSI::NPG::HTS::Samtools->new
-        (arguments => ['view', '-b',
-                       '-T', qq[$data_path/$reference_file],
-                       '-o', qq[irods:$irods_tmp_coll/$data_file.bam]],
-         path      => "$data_path/$data_file.sam")->run;
+      WTSI::NPG::HTS::Samtools->new
+          (arguments => ['view', '-b',
+                         '-T', qq[$data_path/$reference_file],
+                         '-o', qq[irods:$irods_tmp_coll/$data_file.bam]],
+           path      => "$data_path/$data_file.sam")->run;
 
-    if (@groups_added) {
-      # Add some test group permissions
-      foreach my $format (qw(bam cram)) {
-        foreach my $group (qw(group_10 group_100)) {
-          $irods->set_object_permissions('read', $group,
-                                         "$irods_tmp_coll/$data_file.$format");
+      if (@groups_added) {
+        # Add some test group permissions
+        foreach my $format (qw(bam cram)) {
+          foreach my $group (qw(group_10 group_100)) {
+            $irods->set_object_permissions
+              ('read', $group, "$irods_tmp_coll/$data_file.$format");
+          }
         }
       }
     }
@@ -185,7 +189,37 @@ sub align_filter : Test(12) {
   }
 }
 
-sub header : Test(4) {
+sub header : Test(8) {
+ SKIP: {
+    if (not $samtools) {
+      skip 'samtools executable not on the PATH', 8;
+    }
+
+    my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
+
+    foreach my $data_file ($data_file0, $data_file1) {
+      foreach my $format (qw(bam cram)) {
+        my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
+          (collection  => $irods_tmp_coll,
+           data_object => "$data_file.$format",
+           file_format => $format,
+           id_run      => 1,
+           irods       => $irods,
+           position    => 1);
+
+        # 2 * 2 * 1 tests
+        ok($obj->header, "$format header can be read");
+
+        # 2 * 2 * 1 tests
+        cmp_ok(scalar @{$obj->header}, '==', 11,
+               "Correct number of $format header lines") or
+                 diag explain $obj->header;
+      }
+    }
+  } # SKIP samtools
+}
+
+sub is_aligned : Test(4) {
  SKIP: {
     if (not $samtools) {
       skip 'samtools executable not on the PATH', 4;
@@ -193,83 +227,59 @@ sub header : Test(4) {
 
     my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
 
-    foreach my $format (qw(bam cram)) {
-      my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
-        (collection  => $irods_tmp_coll,
-         data_object => "$data_file.$format",
-         file_format => $format,
-         id_run      => 1,
-         irods       => $irods,
-         position    => 1);
+    foreach my $data_file ($data_file0, $data_file1) {
+      foreach my $format (qw(bam cram)) {
+        my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
+          (collection  => $irods_tmp_coll,
+           data_object => "$data_file.$format",
+           file_format => $format,
+           id_run      => 1,
+           irods       => $irods,
+           position    => 1);
 
-      # 2 * 1 tests
-      ok($obj->header, "$format header can be read");
-
-      # 2 * 1 tests
-      cmp_ok(scalar @{$obj->header}, '==', 11,
-             "Correct number of $format header lines") or
-               diag explain $obj->header;
+        # 2 * 2 * 1 tests
+        ok($obj->is_aligned, "$format data are aligned");
+      }
     }
   } # SKIP samtools
 }
 
-sub is_aligned : Test(2) {
+sub reference : Test(4) {
  SKIP: {
     if (not $samtools) {
-      skip 'samtools executable not on the PATH', 2;
+      skip 'samtools executable not on the PATH', 4;
     }
 
     my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
 
-    foreach my $format (qw(bam cram)) {
-      my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
-        (collection  => $irods_tmp_coll,
-         data_object => "$data_file.$format",
-         file_format => $format,
-         id_run      => 1,
-         irods       => $irods,
-         position    => 1);
+    foreach my $data_file ($data_file0, $data_file1) {
+      foreach my $format (qw(bam cram)) {
+        my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
+          (collection  => $irods_tmp_coll,
+           data_object => "$data_file.$format",
+           file_format => $format,
+           id_run      => 1,
+           irods       => $irods,
+           position    => 1);
 
-      # 2 * 1 tests
-      ok($obj->is_aligned, "$format data are aligned");
+        my $regex = qr{\./t\/data\/test_ref.fa}msx;
+        my $filter = sub {
+          my ($line) = @_;
+          return $line =~ m{$regex}msx;
+        };
+
+        # 2 * 2 * 1 tests
+        is($obj->reference($filter), './t/data/test_ref.fa',
+           "$format reference is correct");
+      }
     }
   } # SKIP samtools
 }
 
-sub reference : Test(2) {
+sub update_secondary_metadata : Test(16) {
  SKIP: {
     if (not $samtools) {
-      skip 'samtools executable not on the PATH', 2;
-    }
-
-    my $irods = WTSI::NPG::iRODS->new(strict_baton_version => 0);
-
-    foreach my $format (qw(bam cram)) {
-      my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
-        (collection  => $irods_tmp_coll,
-         data_object => "$data_file.$format",
-         file_format => $format,
-         id_run      => 1,
-         irods       => $irods,
-         position    => 1);
-
-      my $regex = qr{\./t\/data\/test_ref.fa}msx;
-      my $filter = sub {
-        my ($line) = @_;
-        return $line =~ m{$regex}msx;
-      };
-
-      # 2 * 1 tests
-      is($obj->reference($filter), './t/data/test_ref.fa',
-         "$format reference is correct");
-    }
-  } # SKIP samtools
-}
-
-sub update_secondary_metadata : Test(8) {
- SKIP: {
-    if (not $samtools) {
-      skip 'samtools executable not on the PATH', 8;
+      skip 'samtools executable not on the PATH', 16;
     }
 
     my $group_filter = sub {
@@ -304,64 +314,118 @@ sub update_secondary_metadata : Test(8) {
       return $line =~ m{$ref_regex}msx;
     };
 
-    foreach my $format (qw(bam cram)) {
-      my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
-        (collection  => $irods_tmp_coll,
-         data_object => "$data_file.$format",
-         irods       => $irods);
+    foreach my $data_file ($data_file0, $data_file1) {
+      foreach my $format (qw(bam cram)) {
+        my $obj = WTSI::NPG::HTS::HTSFileDataObject->new
+          (collection  => $irods_tmp_coll,
+           data_object => "$data_file.$format",
+           irods       => $irods);
 
-      my @groups_before = $obj->get_groups;
+        my @groups_before = $obj->get_groups;
 
-      my $with_spiked_control = 0;
-      # 2 * 1 tests
-      ok($obj->update_secondary_metadata($schema, $with_spiked_control,
-                                         $ref_filter),
-         'Updating secondary metadata, w/o spiked control');
+        my $with_spiked_control = 0;
+        # 2 * 2 * 1 tests
+        ok($obj->update_secondary_metadata($schema, $with_spiked_control,
+                                           $ref_filter),
+           'Updating secondary metadata, w/o spiked control');
 
-      my @groups_after = $obj->get_groups;
+        my @groups_after = $obj->get_groups;
 
-      my $expected_meta =
-        [{attribute => $ALIGNMENT,                value     => '1'},
-         {attribute => $ID_RUN,                   value     => '3002'},
-         {attribute => $POSITION,                 value     => '3'},
-         {attribute => $LIBRARY_ID,               value     => '60186'},
-         {attribute => $QC_STATE,                 value     => '0'},
-         {attribute => $REFERENCE,
-          value     => './t/data/test_ref.fa'},
-         {attribute => $SAMPLE_COMMON_NAME,
-          value     => 'Streptococcus suis'},
-         {attribute => $SAMPLE_CONSENT_WITHDRAWN, value     => '0'},
-         {attribute => $SAMPLE_NAME,              value     => 'BM308'},
-         {attribute => $STUDY_ACCESSION_NUMBER,   value     => 'ERP000893'},
-         {attribute => $STUDY_ID,                 value     => '244'},
-         {attribute => $STUDY_NAME,
-          value     =>
-          'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
-         {attribute => $STUDY_TITLE,
-          value     =>
-          'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
-         {attribute => $TAG_INDEX,                value     => '1'}];
+        my $tag0_expected_meta =
+          [{attribute => $ALIGNMENT,                value     => '1'},
+           {attribute => $ID_RUN,                   value     => '3002'},
+           {attribute => $POSITION,                 value     => '3'},
+           {attribute => $LIBRARY_ID,               value     => '60186'},
+           {attribute => $LIBRARY_ID,               value     => '60188'},
+           {attribute => $LIBRARY_ID,               value     => '60190'},
+           {attribute => $LIBRARY_ID,               value     => '60192'},
+           {attribute => $LIBRARY_ID,               value     => '60194'},
+           {attribute => $LIBRARY_ID,               value     => '60196'},
+           {attribute => $LIBRARY_ID,               value     => '60198'},
+           {attribute => $LIBRARY_ID,               value     => '60200'},
+           {attribute => $LIBRARY_ID,               value     => '60202'},
+           {attribute => $LIBRARY_ID,               value     => '60204'},
+           {attribute => $LIBRARY_ID,               value     => '60206'},
+           {attribute => $LIBRARY_ID,               value     => '60208'},
+           {attribute => $REFERENCE,
+            value     => './t/data/test_ref.fa'},
+           {attribute => $SAMPLE_COMMON_NAME,
+            value     => 'Streptococcus suis'},
+           {attribute => $SAMPLE_CONSENT_WITHDRAWN, value     => '0'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM308'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM315'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM321'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM329'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM334'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM345'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM346'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM357'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM358'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM362'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM366'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM373'},
+           {attribute => $STUDY_ACCESSION_NUMBER,   value     => 'ERP000893'},
+           {attribute => $STUDY_ID,                 value     => '244'},
+           {attribute => $STUDY_NAME,
+            value     =>
+            'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
+           {attribute => $STUDY_TITLE,
+            value     =>
+            'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
+           {attribute => $TAG_INDEX,                value     => '0'}];
 
-      my $meta = [grep { $_->{attribute} !~ m{_history$} }
-                  @{$obj->metadata}];
-      # 2 * 1 tests
-      is_deeply($meta, $expected_meta, 'Secondary metadata updated correctly')
-        or diag explain $meta;
+        my $tag1_expected_meta =
+          [{attribute => $ALIGNMENT,                value     => '1'},
+           {attribute => $ID_RUN,                   value     => '3002'},
+           {attribute => $POSITION,                 value     => '3'},
+           {attribute => $LIBRARY_ID,               value     => '60186'},
+           {attribute => $QC_STATE,                 value     => '0'},
+           {attribute => $REFERENCE,
+            value     => './t/data/test_ref.fa'},
+           {attribute => $SAMPLE_COMMON_NAME,
+            value     => 'Streptococcus suis'},
+           {attribute => $SAMPLE_CONSENT_WITHDRAWN, value     => '0'},
+           {attribute => $SAMPLE_NAME,              value     => 'BM308'},
+           {attribute => $STUDY_ACCESSION_NUMBER,   value     => 'ERP000893'},
+           {attribute => $STUDY_ID,                 value     => '244'},
+           {attribute => $STUDY_NAME,
+            value     =>
+            'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
+           {attribute => $STUDY_TITLE,
+            value     =>
+            'Discovery of sequence diversity in Streptococcus suis (Vietnam)'},
+           {attribute => $TAG_INDEX,                value     => '1'}];
 
-    SKIP: {
-        if (not @groups_added) {
-          skip 'iRODS groups were not added', 2;
+        my $meta = [grep { $_->{attribute} !~ m{_history$} }
+                    @{$obj->metadata}];
+
+        # 2 * 2 * 1 tests
+        if ($obj->tag_index == 0) {
+          is_deeply($meta, $tag0_expected_meta,
+                    'Secondary metadata updated correctly, tag 0')
+            or diag explain $meta;
         }
         else {
-          my $expected_groups_before = ['group_10', 'group_100'];
-          # 2 * 1 tests
-          is_deeply(\@groups_before, $expected_groups_before,
-                    'Groups before update') or diag explain \@groups_before;
+          is_deeply($meta, $tag1_expected_meta,
+                    'Secondary metadata updated correctly, tag 1')
+            or diag explain $meta;
+        }
 
-          my $expected_groups_after = ['group_244'];
-          # 2 * 1 tests
-          is_deeply(\@groups_after, $expected_groups_after,
-                    'Groups after update') or diag explain \@groups_after;
+      SKIP: {
+          if (not @groups_added) {
+            skip 'iRODS groups were not added', 2;
+          }
+          else {
+            my $expected_groups_before = ['group_10', 'group_100'];
+            # 2 * 2 * 1 tests
+            is_deeply(\@groups_before, $expected_groups_before,
+                      'Groups before update') or diag explain \@groups_before;
+
+            my $expected_groups_after = ['group_244'];
+            # 2 * 2 * 1 tests
+            is_deeply(\@groups_after, $expected_groups_after,
+                      'Groups after update') or diag explain \@groups_after;
+          }
         }
       } # SKIP groups_added
     }
