d798c5ca149b291142b85835d05734a9d3421e21
Keith James <kdj@sanger.ac.uk>
2016-09-15 14:27:19 +0100

    Test the content of the added EBI submission metadata.

--- t/lib/WTSI/NPG/DataSub/MetaUpdaterTest.pm
+++ t/lib/WTSI/NPG/DataSub/MetaUpdaterTest.pm
@@ -51,14 +51,16 @@ sub setup_test : Test(setup) {
 sub teardown_test : Test(teardown) {
   my $irods = WTSI::NPG::iRODS->new(environment          => \%ENV,
                                     strict_baton_version => 0);
-  $irods->remove_collection($irods_tmp_coll);
+  # $irods->remove_collection($irods_tmp_coll);
 }
 
-sub update_submission_metadata : Test(2) {
+sub update_submission_metadata : Test(22) {
   my $irods = WTSI::NPG::iRODS->new(environment          => \%ENV,
                                     strict_baton_version => 0);
   my $meta_updater = WTSI::NPG::DataSub::MetaUpdater->new(irods => $irods);
 
+  # Dummy subtrack database data for 3 files with distinct name/MD5
+  # pairs
   my @expected_valid = ({
                          ebi_run_acc => "ERR1609600",
                          ebi_sub_acc => "ERA697039",
@@ -85,10 +87,30 @@ sub update_submission_metadata : Test(2) {
   my @files_valid = WTSI::NPG::DataSub::SubtrackClient->new
     (dbh => $dbh_valid)->query_submitted_files;
 
+  # These 3 files should have submission metadata added
   cmp_ok($meta_updater->update_submission_metadata("$irods_tmp_coll/valid",
                                                    \@files_valid),
          '==', 3, "Updated metadata on unambiguous name/MD5 results");
 
+  foreach my $item (@expected_valid) {
+    my $path = "$irods_tmp_coll/valid/" . $item->{file_name};
+    my $obj = WTSI::NPG::iRODS::DataObject->new($irods, $path);
+
+    is($obj->get_avu('ebi_run_acc')->{value}, $item->{ebi_run_acc},
+       'EBI run accession AVU') or diag explain $obj->metadata;
+
+    is($obj->get_avu('ebi_sub_acc')->{value}, $item->{ebi_sub_acc},
+       'EBI submission accession AVU') or diag explain $obj->metadata;
+
+    is($obj->get_avu('ebi_sub_date')->{value}, $item->{timestamp},
+       'EBI submission date AVU') or diag explain $obj->metadata;
+
+    is($obj->get_avu('ebi_sub_md5')->{value}, $item->{md5},
+       'EBI submission MD5 AVU') or diag explain $obj->metadata;
+  }
+
+  # Dummy subtrack database data for 2 files with the same name/MD5
+  # pair
   my @expected_invalid = ({
                            ebi_run_acc => "ERR1609600",
                            ebi_sub_acc => "ERA697039",
@@ -101,9 +123,20 @@ sub update_submission_metadata : Test(2) {
   my @files_invalid = WTSI::NPG::DataSub::SubtrackClient->new
     (dbh => $dbh_invalid)->query_submitted_files;
 
+  # These files should not have submission metadata added because they
+  # are duplicates
   cmp_ok($meta_updater->update_submission_metadata("$irods_tmp_coll/invalid",
                                                    \@files_invalid),
          '==', 0, "No metadata update on ambiguous name/MD5 results");
+
+  foreach my $coll ("$irods_tmp_coll/invalid/", "$irods_tmp_coll/invalid/x/") {
+    my $path = "$coll/a.txt";
+    my $obj = WTSI::NPG::iRODS::DataObject->new($irods, $path);
+
+    foreach my $attr (qw[ebi_run_acc ebi_sub_acc ebi_sub_date ebi_sub_md5]) {
+      is($obj->get_avu($attr), undef, "$attr absent from ambiguous path");
+    }
+  }
 }
 
 sub _make_mock_dbh {
