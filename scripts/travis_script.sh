8cbf7e4c432800ea1900e0547d5b94c80fcf2cfb
iainrb <ib5@sanger.ac.uk>
2016-10-31 08:59:01 +0000

    Remove UUID_DEBUG

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -5,7 +5,6 @@ set -e -x
 export TEST_AUTHOR=1
 export WTSI_NPG_iRODS_Test_irodsEnvFile=$HOME/.irods/.irodsEnv
 export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
-export UUID_DEBUG=1
 
 perl Build.PL
 ./Build clean
