d3ae34dbe66203b0a901937e98513e5dfd606cd7
Keith James <kdj@sanger.ac.uk>
2017-07-19 16:58:33 +0100

    Added a new build script for ONT platforms.

--- scripts/travis_script.sh
+++ scripts/travis_script.sh
@@ -7,6 +7,10 @@ export TEST_WITH_H5REPACK=1
 export WTSI_NPG_iRODS_Test_irodsEnvFile=$HOME/.irods/.irodsEnv
 export WTSI_NPG_iRODS_Test_IRODS_ENVIRONMENT_FILE=$HOME/.irods/irods_environment.json
 
+perl BuildONT.PL
+./BuildONT clean
+./BuildONT test
+
 perl Build.PL
 ./Build clean
 ./Build test
