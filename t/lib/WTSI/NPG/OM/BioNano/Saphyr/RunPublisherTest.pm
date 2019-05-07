6a7c2acf9bfb8c06662b3d445241287aff9136b8
Keith James <kdj@sanger.ac.uk>
2019-05-07 17:31:32 +0100

    Extended tests to include sample metadata.

--- t/lib/WTSI/NPG/OM/BioNano/Saphyr/RunPublisherTest.pm
+++ t/lib/WTSI/NPG/OM/BioNano/Saphyr/RunPublisherTest.pm
@@ -101,7 +101,7 @@ sub teardown_test : Test(teardown) {
   $irods->remove_collection($irods_tmp_coll);
 }
 
-sub publish_files : Test(56) {
+sub publish_files : Test(88) {
   my $irods = WTSI::NPG::iRODS->new(environment          => \%ENV,
                                     strict_baton_version => 0);
 
@@ -146,11 +146,10 @@ sub publish_files : Test(56) {
   check_common_metadata($irods, @observed_paths);
   check_primary_metadata($irods, @observed_paths);
 
- TODO: {
-    todo_skip 'ML warehouse support for Saphyr not yet available', 12;
+  check_study_metadata($irods, @observed_paths);
+  check_sample_metadata($irods, @observed_paths);
 
-    check_study_metadata($irods, @observed_paths);
-  }
+
 }
 
 sub observed_data_objects {
@@ -191,12 +190,59 @@ sub check_primary_metadata {
        $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_CHIP_FLOWCELL,
        $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_SAMPLE_NAME,
        $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_PROJECT_NAME,
-       $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_EXPERIMENT_NAME);
+       $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_EXPERIMENT_NAME,
+       $LIBRARY_ID);
 
     foreach my $attr(@attrs) {
       my @avu = $obj->find_in_metadata($attr);
       cmp_ok(scalar @avu, '==', 1, "$file_name $attr metadata present");
     }
+
+    deep_observed_vs_expected
+        ([$obj->find_in_metadata($WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_RUN_UID)],
+         [{attribute => $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_RUN_UID,
+           value     => '8ae847ee-dfa4-473d-894e-ffb245f8d8c7'}],
+         'Run UID has expected value');
+
+    deep_observed_vs_expected
+        ([$obj->find_in_metadata($WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_CHIP_SERIALNUMBER)],
+         [{attribute => $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_CHIP_SERIALNUMBER,
+           value     => 'KHPZDTGLPQJGPNWU'}],
+         'Chip serial number has expected value');
+
+    my ($flowcell) = $path =~
+        m{/8ae847ee-dfa4-473d-894e-ffb245f8d8c7[.]KHPZDTGLPQJGPNWU[.](\d)[.]}msx;
+
+    my ($sample_name, $saphyr_sample_name);
+    if ($flowcell eq '1') {
+      $sample_name = '5290STDY7575139'; # From warehouse
+      $saphyr_sample_name = 'fGadMor1'; # From Saphyr database
+    }
+    elsif ($flowcell eq '2') {
+      $sample_name = '4616STDY6965619'; # From warehouse
+      $saphyr_sample_name = '25G257155090'; # From Saphyr database
+    }
+    else {
+      fail "Unexpected Saphyr flowcell '$flowcell'";
+    }
+
+    deep_observed_vs_expected
+        ([$obj->find_in_metadata($WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_CHIP_FLOWCELL)],
+         [{attribute => $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_CHIP_FLOWCELL,
+           value     => $flowcell}],
+         'Chip flowcell has expected value');
+
+    deep_observed_vs_expected
+        ([$obj->find_in_metadata($SAMPLE_NAME)],
+         [{attribute => $SAMPLE_NAME,
+           value     => $sample_name}],
+         'Saphyr sample name has expected value');
+
+    deep_observed_vs_expected
+        ([$obj->find_in_metadata($WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_SAMPLE_NAME)],
+         [{attribute => $WTSI::NPG::OM::BioNano::Saphyr::Annotator::SAPHYR_SAMPLE_NAME,
+           value     => $saphyr_sample_name}],
+         'Saphyr sample name has expected value');
   }
 }
 
@@ -212,6 +258,27 @@ sub check_study_metadata {
       cmp_ok(scalar @avu, '==', 1, "$file_name $attr metadata present");
     }
   }
+}
+
+sub check_sample_metadata {
+  my ($irods, @paths) = @_;
+
+  foreach my $path (@paths) {
+    my $obj = WTSI::NPG::HTS::DataObject->new($irods, $path);
+    my $file_name = fileparse($obj->str);
+
+    foreach my $attr ($SAMPLE_ID, $SAMPLE_DONOR_ID, $SAMPLE_NAME,
+                      $SAMPLE_SUPPLIER_NAME) {
+      my @avu = $obj->find_in_metadata($attr);
+      cmp_ok(scalar @avu, '==', 1, "$file_name $attr metadata present");
+    }
+  }
+}
+
+sub deep_observed_vs_expected {
+  my ($observed, $expected, $message) = @_;
+
+  is_deeply($observed, $expected, $message) or diag explain $observed;
 }
 
 1;
