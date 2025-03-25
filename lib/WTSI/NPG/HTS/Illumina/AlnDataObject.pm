8634a0b6ccbc514e7a4233881f23dd34fffeef29
Marina Gourtovaia <mg8@sanger.ac.uk>
2025-03-25 13:41:58 +0000

    Added "dehumanised" primary metadata

--- lib/WTSI/NPG/HTS/Illumina/AlnDataObject.pm
+++ lib/WTSI/NPG/HTS/Illumina/AlnDataObject.pm
@@ -79,6 +79,7 @@ sub BUILD {
     $ALT_TARGET,
     $COMPONENT,
     $COMPOSITION,
+    $DEHUMANISED,
     $ID_PRODUCT,
     $ID_RUN,
     $IS_PAIRED_READ,
@@ -140,7 +141,7 @@ sub is_aligned {
 
                This will give incorrect results if the reference path
                contains whitespace.
-  Returntype : Bool
+  Returntype : Str | undefined value
 
 =cut
 
@@ -154,6 +155,26 @@ sub reference {
   }
 
   return $reference;
+}
+
+=head2 dehumanised
+
+  Example    : my $dh_meta_value = $obj->dehumanised();
+
+  Description: Returns a value for assigning C<dehumanised> iRODS metadata. 
+
+               See details in the documentation for C<dehumanising_method>
+               in C<WTSI::NPG::HTS::HeaderParser>.
+
+  Returntype : Str | undefined value
+
+=cut
+
+sub dehumanised {
+  my $self = shift;
+  my $dehumanised = WTSI::NPG::HTS::HeaderParser->new
+                   ->dehumanising_method($self->header);
+  return $dehumanised;
 }
 
 sub _build_composition {
@@ -355,10 +376,11 @@ overrides some base class behaviour to introduce:
 
 Keith James <kdj@sanger.ac.uk>
 
+Marina Gourtovaia <mg8@sanger.ac.uk>
+
 =head1 COPYRIGHT AND DISCLAIMER
 
-Copyright (C) 2015, 2016, 2017, 2018, 2020 Genome Research Limited.
- All Rights Reserved.
+Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020, 2023, 2025 Genome Research Ltd.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the Perl Artistic License or the GNU General
